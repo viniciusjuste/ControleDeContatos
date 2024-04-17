fb7feb0ecbfcfdf32aee8257a5aca09b2a2dba69
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-04-16 22:36:57 -0300

    update

--- obj/Debug/net8.0/ControleDeContatos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ControleDeContatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4599f749b891065145f69577fb8cb1472cb8e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
